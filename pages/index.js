2b452b2c84815b2f1ed65dbb0896d447c4cd17f1
Theo Lampert <theolampert@gmail.com>
2019-09-20 10:58:23 +0200

    initial example

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Flickity from "react-flickity-component";
+
+import "../styles.css";
+import "flickity/css/flickity.css";
+
+function Carousel() {
+  return (
+    <Flickity>
+      <img src="https://placeimg.com/640/480/animals" />
+      <img src="https://placeimg.com/640/480/animals" />
+    </Flickity>
+  );
+}
+
+function Home() {
+  return (
+    <div className="App">
+      <h1>React Flickity</h1>
+      <Carousel />
+    </div>
+  );
+}
+
+export default Home
